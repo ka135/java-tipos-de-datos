dc4c5a53a36ba1db93f740a899f7a1e0a482d94d
ka135 <karolinasuarezl11@gmail.com>
2023-08-11 14:21:39 -0500

    se adiciona el modulo de la potencia

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,33 +8,51 @@ public class Calculadora {
         int numero1 = lector.nextInt();
         System.out.println("Ingrese el número 2");
         int numero2 = lector.nextInt();
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /, %, ^)");
         char operacion = lector.next().charAt(0);
 
         switch (operacion) {
-            case '+' -> {
+            case '+':
                 // Operación suma:
                 int suma = numero1 + numero2;
                 System.out.println("La suma es " + suma);
-            }
-            case '-' -> {
+                break;
+            case '-':
                 // Operación resta:
                 int resta = numero1 - numero2;
                 System.out.println("La resta es " + resta);
-            }
-            case '*' -> {
+                break;
+            case '*':
                 // Operación multiplicación:
                 int multiplicacion = numero1 * numero2;
                 System.out.println("La multiplicación es " + multiplicacion);
-            }
-            case '/' -> {
+                break;
+            case '/':
                 // Operación división:
                 /* Es necesario que uno de los números que intervienen en la división sea double
                 para que el resultado decimal sea aproximado correctamente. **/
                 double division = (double) numero1 / numero2;
                 System.out.println("La división es " + division);
-            }
-            default -> System.out.println("Operación inválida");
+                break;
+            case '%':
+                // Operación residuo división:
+                double residuo = (double) numero1 % numero2;
+                System.out.println("El residuo división es " + residuo);
+                break;
+            case '^':
+                // Operación potencia:
+                double potencia = pot(numero1, numero2);
+                System.out.println("La división es " + potencia );
+                break;
+            default:
+                System.out.println("Operación inválida");
         }
-    }
 }
+
+    private static double pot(int numero1, int numero2) {
+        float res=1;
+        for(int i=1;i<=numero2;i++)
+            res*=numero1;
+        return res;
+    }
+    }
